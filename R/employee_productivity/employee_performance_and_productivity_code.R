165a3ef02876d7ec50bcd6b3a58509aab680b924
Shinin Varongchayakul <shivarong@gmail.com>
2025-04-10 20:21:02 +0700

    Complete EDA part 1

--- R/employee_productivity/employee_performance_and_productivity_code.R
+++ R/employee_productivity/employee_performance_and_productivity_code.R
@@ -12,11 +12,13 @@
 install.packages("tidyverse")
 install.packages("tidymodels")
 install.packages("data.table")
+install.packages("dtplyr")
 
 ## Load
 library(tidyverse)
 library(tidymodels)
 library(data.table)
+library(dtplyr)
 
 
 # -------------------------
@@ -24,11 +26,8 @@ library(data.table)
 
 # Load the dataset
 
-## Store raw URL
-raw_url <- "https://raw.githubusercontent.com/svarong-port/data-analytics-projects/refs/heads/main/employee_productivity/employee_performance_and_productivity_extended_dataset.csv"
-
 ## Load the data with fread()
-prod <- fread(raw_url)
+prod <- fread("employee_performance_and_productivity_extended_dataset.csv")
 
 ## Preview the data
 head(prod)
@@ -85,7 +84,53 @@ glimpse(prod_cleaned)
 today_date <- as.Date("2025-04-10")
 
 ### Update
-prod_cleaned[, Years_At_Company := as.integer(format(today_date, "%Y") - year(Hire_Date))]
+prod_cleaned[, Years_At_Company := as.integer(format(today_date, "%Y")) - year(Hire_Date)]
 
 ## Check the result
 glimpse(prod_cleaned)
+
+
+# -------------------------
+
+
+# EDA, part I - explore sample characteristics
+
+## Create a copy of the dataset for EDA
+prod_eda <- copy(prod_cleaned)
+
+
+## Compute the number of employees by department
+prod_eda[,
+         .(Total = .N,
+           Percent = .N / nrow(prod_eda) * 100),
+         by = Department][order(-Percent)]
+
+
+## Compute the number of employees by gender
+prod_eda[,
+         .(Total = .N,
+           Percent = .N / nrow(prod_eda) * 100),
+         by = Gender][order(-Percent)]
+
+
+## Compute the number of employees by education level
+
+### Reorder the education level factors
+prod_eda[, Education_Level := factor(Education_Level,
+                                     levels = c("High School",
+                                                "Bachelor",
+                                                "Master",
+                                                "PhD"))]
+
+### Compute
+prod_eda[, 
+         .(Total = .N,
+           Percent = .N / nrow(prod_eda) * 100),
+         by = Education_Level][order(Education_Level)]
+
+
+## Compute the number of employees by team size
+prod_eda[, 
+         .(Total = .N,
+           Percent = .N / nrow(prod_eda) * 100),
+         by = Team_Size][order(Team_Size)]
