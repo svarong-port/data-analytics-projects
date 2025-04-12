fd69fc50cc7c2800e28515ce29feb193affd72d8
Shinin Varongchayakul <shivarong@gmail.com>
2025-04-12 11:48:26 +0700

    Add code

--- R/employee_productivity/employee_performance_and_productivity_code.R
+++ R/employee_productivity/employee_performance_and_productivity_code.R
@@ -6,7 +6,7 @@
 ## Retrieved Date: 10 Apr 2025
 
 
-# Install and load packages
+# 1. Install and load packages
 
 ## Install
 install.packages("tidyverse") # data manipulation
@@ -26,7 +26,9 @@ library(lubridate)
 # -------------------------
 
 
-# Load the dataset
+# 2. Prepare the dataset
+
+## 2.1 Load the dataset
 
 ## Store raw CSV URL from GitHub
 raw_url <- "https://raw.githubusercontent.com/svarong-port/data-analytics-projects/refs/heads/main/R/employee_productivity/employee_performance_and_productivity_extended_dataset.csv"
@@ -44,21 +46,21 @@ glimpse(prod)
 # -------------------------
 
 
-# Prepare the dataset
+# 2.2 Clean the dataset
 
-## Remove unnecessary columns
+## 2.2.1 Remove unnecessary columns
 prod_cleaned <- prod[, -"Employee_ID"]
 
 ## Check the result
 glimpse(prod_cleaned)
 
 
-## Convert character columns to factor
+## 2.2.2 Convert character columns to factor
 
-### Select character columns
+## Select character columns
 char_cols <- names(prod_cleaned)[sapply(prod_cleaned, is.character)]
 
-### Convert to factor
+## Convert to factor
 prod_cleaned[, 
              (char_cols) := lapply(.SD, as.factor),
              .SDcols = char_cols]
@@ -67,7 +69,7 @@ prod_cleaned[,
 glimpse(prod_cleaned)
 
 
-## Convert `Resigned` to factor
+## 2.2.3 Convert `Resigned` to factor
 prod_cleaned$Resigned <- factor(prod_cleaned$Resigned,
                                 levels = c(FALSE, TRUE),
                                 labels = c("No", "Yes"))
@@ -76,14 +78,14 @@ prod_cleaned$Resigned <- factor(prod_cleaned$Resigned,
 glimpse(prod_cleaned)
 
 
-## Convert `Hire_Date` to Date type
+## 2.2.4 Convert `Hire_Date` to Date type
 prod_cleaned[, Hire_Date := as.Date(Hire_Date)]
 
 ## Check the result
 glimpse(prod_cleaned)
 
 
-## Update `Years_At_Company`
+## 2.2.5 Update `Years_At_Company`
 
 ### Store today's date
 today_date <- as.Date("2025-04-10")
@@ -95,14 +97,25 @@ prod_cleaned[, Years_At_Company := as.integer(format(today_date, "%Y")) - year(H
 glimpse(prod_cleaned)
 
 
+## 2.2.6 Create `High_Performer` column
+prod_cleaned[, High_Performer := factor(ifelse(Performance_Score >= 4, "Yes", "No"), levels = c("Yes", "No"))]
+
+## Check the result
+glimpse(prod_cleaned)
+
+
 # -------------------------
 
 
-# EDA, part I - explore sample characteristics
+# 3. EDA
 
 ## Create a copy of the dataset for EDA
 prod_eda <- copy(prod_cleaned)
 
+
+## 3.1 Demographic variables
+
+### 3.1.1 Gender
 
 ## Compute the number of employees by gender
 prod_eda[,
@@ -136,6 +149,8 @@ prod_eda |>
   theme_classic()
 
 
+### 3.1.2 Age
+
 ## Compute the number of employees by education level
 
 ### Reorder the education level factors
@@ -152,7 +167,7 @@ prod_eda[,
          by = Education_Level][order(Education_Level)]
 
 
-
+### 3.1.3 Education level
 
 ## Compute the number of employees by department
 prod_eda[,
@@ -164,7 +179,9 @@ prod_eda[,
 # -------------------------
 
 
-# EDA, part 2
+## 3.2 Job-related variables
+
+### 3.2.1 Department
 
 # Compute the number of employees by department
 prod_eda[,
@@ -172,6 +189,8 @@ prod_eda[,
            Percent = .N / nrow(prod_eda) * 100),
          by = Department][order(-Percent)]
 
+
+### 3.2.2 Team size
 
 ## Compute the number of employees by team size
 prod_eda[, 
@@ -185,12 +204,17 @@ prod_eda[,
            Percent = .N / nrow(prod_eda) * 100),
          by = Job_Title][order(Job_Title)]
 
+
+### 3.3.3 Job title
+
 ## Compute the number of employees by hire date
 
 ### Find hire year and hire month
 prod_eda[, `:=` (Hire_Year = year(Hire_Date),
                  Hire_Month_Name = month(Hire_Date))]
 
+
+### 3.3.4 Hire date
 
 ### Compute the number of hires per year
 hires_by_year <- prod_eda[,
@@ -235,17 +259,36 @@ hires_by_month
 ### Plot the number of employees by month
 hires_by_month |>
 
-  #### Aesthetic mapping
+  ## Aesthetic mapping
   ggplot(aes(x = Hire_Month_Name,
              y = Avg_Hires)) +
   
-  #### Instantiate a line plot
+  ## Instantiate a line plot
   geom_col(fill = "steelblue") +
   
-  #### Add text elements
+  ## Add text elements
   labs(title = "Average Number of Employees Hired by Month",
        x = "Month",
        y = "Average Hires") +
   
-  #### Adjust classic theme
+  ## Adjust classic theme
   theme_classic()
+
+
+### 3.2.5 Years at company
+prod_eda[, .(Count = .N,
+             Percent = .N / nrow(prod_eda) * 100),
+         by = Years_At_Company] |>
+  
+  ## Aesthetic mapping
+
+### 3.2.6 Resignation status
+
+## Compute the number of employees by resignation status
+prod_eda[,
+         .(Count = .N,
+             Percent = .N / nrow(prod_eda) * 100),
+         by = Resigned]
+
+
+### 3.2.7 Remote work frequency
