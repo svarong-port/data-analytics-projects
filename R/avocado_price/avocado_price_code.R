ae2891afb17c9fa71ad567313a7a9c1136096f4f
Shinin Varongchayakul <shivarong@gmail.com>
2025-05-05 22:03:41 +0700

    Initial commit

--- R/avocado_price/avocado_price_code.R
+++ R/avocado_price/avocado_price_code.R
@@ -55,13 +55,13 @@ library(lubridate)
 # ----------------------------------------------------------------
 
 
-## 2. Load the dataset
+## 2. Load and prepare the dataset
 
-## Load
-avocados <- read.csv("avocado_price_dataset.csv")
+## 2.1 Load
+avocados <- read_csv("avocado_price_dataset.csv")
 
-## Preview the first 6 row
-head(avocados)
+## Preview the first 10 row
+head(avocados, 10)
 
 ## View the structure
 glimpse(avocados)
@@ -70,24 +70,59 @@ glimpse(avocados)
 ## - There are 18,249 records and 14 features.
 
 
-# ----------------------------------------------------------------
-
-
-## 3. Prepare the dataset
+## 2.2 Prepare the dataset
 
 ## Create a copy of the dataset
 avd_cleaned <- avocados
 
 
-## Convert `Date` to Date
-avd_cleaned$Date <- as.Date(avd_cleaned$Date)
+## 2.2.1 Drop index column
+
+## Drop
+avd_cleaned$...1 <- NULL
 
 ## Check the results
 glimpse(avd_cleaned)
 
 
-## Extract month from `Date`
-avd_cleaned$month <- format(avd_cleaned$Date, "%B")
+## 2.2.2 Rename the columns
+
+### Rename
+avd_cleaned <- avd_cleaned |>
+  
+  ## Rename
+  rename(date = Date,
+         avg_price = AveragePrice,
+         vol_total = `Total Volume`,
+         vol_4046 = `4046`,
+         vol_4225 = `4225`,
+         vol_4770 = `4770`,
+         bags_total = `Total Bags`,
+         bags_small = `Small Bags`,
+         bags_large = `Large Bags`,
+         bags_xl = `XLarge Bags`,
+         pricing_strategy = type,
+         year = year,
+         region = region)
+
+## Check the results
+glimpse(avd_cleaned)
+
+
+## 2.2.3 Convert `date` to Date
+
+## Convert 
+avd_cleaned$date <- as.Date(avd_cleaned$date)
+
+## Check the results
+glimpse(avd_cleaned)
+
+
+## Extract month from `date`
+
+## Extract
+avd_cleaned$month <- format(avd_cleaned$date,
+                            "%B")
 
 ## Convert `month` to factor
 avd_cleaned$month <- factor(avd_cleaned$month,
@@ -98,7 +133,7 @@ avd_cleaned$month <- factor(avd_cleaned$month,
 glimpse(avd_cleaned)
 
 
-## Group region into more meaningful regions
+## Group `region` into more meaningful regions
 
 ## Create a data frame for region grouping
 region_groups <- data.frame(
@@ -113,7 +148,7 @@ region_groups <- data.frame(
     "SanFrancisco", "Seattle", "SouthCarolina", "SouthCentral", "Southeast", "Spokane",
     "StLouis", "Syracuse", "Tampa", "TotalUS", "West", "WestTexNewMexico"
   ),
-  Region.Group = c(
+  region_group = c(
     "Northeast", "South", "Northeast", "West", "Northeast", "Northeast",
     "West", "South", "Midwest", "Midwest", "Midwest", "South",
     "West", "Midwest", "Midwest", "Midwest", "Northeast", "Northeast",
@@ -135,8 +170,9 @@ avd_cleaned <- left_join(avd_cleaned,
 glimpse(avd_cleaned)
 
 
+## 2.2.6 Convert character columns to factor
 
-## Convert `type`, `region`, and `Region.Group` to factor
+## Convert
 avd_cleaned <- avd_cleaned |>
   
   ## Convert to factor
@@ -147,21 +183,28 @@ avd_cleaned <- avd_cleaned |>
 glimpse(avd_cleaned)
 
 
-## Drop `X`
-avd_cleaned$X <- NULL
+## 2.2.7 Re-arrange the columns
+
+## Re-arrange
+avd_cleaned <- avd_cleaned |>
+  
+  ## Select the columns
+  select(date, year, month,
+         region_group, region,
+         vol_total, vol_4046, vol_4225, vol_4770,
+         bags_total, bags_small, bags_large, bags_xl,
+         pricing_strategy, avg_price)
 
 ## Check the results
 glimpse(avd_cleaned)
 
-## Comments:
-## - There are 18,249 records and 15 features.
-## - No rows have been dropped.
 
-
-## Check for missing values
+## 2.2.8 Check for missing values
 anyNA(avd_cleaned)
 
-## Comments: No missing values found.
+## Comments:
+## - No missing values found.
+
 
 
 # ----------------------------------------------------------------
@@ -171,31 +214,11 @@ anyNA(avd_cleaned)
 
 ## 3.1 Overview
 
-## See summary stats
+## 3.1.1 See summary stats
 summary(avd_cleaned)
 
 
-## Visualise the distribution of `AveragePrice`
-avd_cleaned |>
-  
-  ## Aesthetic mapping
-  ggplot(aes(x = AveragePrice)) +
-  
-  ## Instantiate a box plot
-  geom_histogram(binwidth = 0.1) +
-  
-  ## Add text elements
-  labs(title = "Distribution of Average Price") +
-  
-  ## Adjust theme to minimal
-  theme_minimal()
-
-## Comments:
-## - The distribution appears normally distributed.
-## - This is in line with the `summary()` results which shows that median (1.360) and mean (1.391) are very close to one another.
-
-
-## Create a correlation matrix
+## 3.1.2 Create a correlation matrix
 avd_cleaned |>
   
   ## Select numeric columns
@@ -207,15 +230,36 @@ avd_cleaned |>
   ## Visualise
   ggcorrplot(lab = TRUE,
              type = "lower",
-             colors = c("blue", "white", "red"),
+             colors = c("skyblue", "white", "darkgreen"),
              lab_size = 2)
 
 ## Comments:
-## - The correlations between `AveragePrice` and other variables range from 0.08 to 0.21.
-## - `year` shows the weakest correlation at 0.08.
-## - `X4046` shows the strongest correlation at -0.21.
+## - The correlations between `avg_price` and other variables range from 0.08 to 0.21.
+## - `year` shows the weakest correlation at 0.09.
+## - `vol_4046` shows the strongest correlation at -0.21.
 ## - The magnitude of the correlations suggest that price may stem from a combination of factors, rather than any single factor alone.
-## - Additionally, many factors overlap greatly, for example, `Total.Volume` and other factor related to the number of sales such as `X4046`. 
+## - Additionally, many factors overlap greatly, for example, `vol_total` and other factor related to the number of sales such as `vol_4046`.
+
+
+## 3.1.3 Visualise the distribution of price
+avd_cleaned |>
+  
+  ## Aesthetic mapping
+  ggplot(aes(x = avg_price)) +
+  
+  ## Instantiate a box plot
+  geom_histogram(binwidth = 0.1,
+                 fill = "#7ea122") +
+  
+  ## Add text elements
+  labs(title = "Distribution of Average Price") +
+  
+  ## Adjust theme to minimal
+  theme_minimal()
+
+## Comments:
+## - The distribution appears normally distributed.
+## - This is in line with the `summary()` results which shows that median (1.370) and mean (1.406) are very close to one another.
 
 
 ## 3.2 Price vs time
@@ -227,32 +271,32 @@ avd_cleaned |>
   group_by(year) |>
   
   ## Compute mean price per year
-  summarise(MeanPrice = mean(AveragePrice)) |>
+  summarise(mean_price = mean(avg_price)) |>
   
   ## Ungroup
   ungroup() |>
   
   ## Aesthetic mapping
   ggplot(aes(x = year,
-             y = MeanPrice)) +
+             y = mean_price)) +
   
   ## Instantiate a line plot
-  geom_line(color = "blue") +
+  geom_line(color = "#7ea122") +
   
   ## Add annotation
-  geom_text(aes(label = paste(round(MeanPrice, 2), "USD")),
+  geom_text(aes(label = paste(round(mean_price, 2), "USD")),
             nudge_y = 0.02,
             size = 3) +
   
   ## Add text elements
-  labs(title = "Avocado Price Across the Years",
-       x = "Year",
-       y = "Average Price") +
+  labs(title = "Distribution of Average Price",
+       x = "Average Price (USD)",
+       y = "Frequency") +
   
   ## Adjust theme to minimal
   theme_minimal()
 
-## Comments
+## Comments:
 ## - Avocado price was highest in 2017, at 1.52 USD, and lowest in 2016, at 1.34 USD.
 
 
@@ -263,14 +307,14 @@ avd_cleaned |>
   group_by(month) |>
   
   ## Compute mean price per month
-  summarise(MeanPrice = mean(AveragePrice)) |>
+  summarise(mean_price = mean(avg_price)) |>
   
   ## Ungroup
   ungroup() |>
   
   ## Aesthetic mapping
   ggplot(aes(x = month,
-             y = MeanPrice,
+             y = mean_price,
              fill = month)) +
   
   ## Instantiate a bar plot
@@ -282,7 +326,7 @@ avd_cleaned |>
             linewidth = 1) +
   
   ## Add annotation
-  geom_text(aes(label = paste(round(MeanPrice, 2), "USD")),
+  geom_text(aes(label = paste(round(mean_price, 2), "USD")),
             position = position_dodge(width = 0.9),
             vjust = -0.5,
             size = 3) +
@@ -293,7 +337,11 @@ avd_cleaned |>
        y = "Average Price") +
   
   ## Adjust theme to minimal
-  theme_minimal()
+  theme_minimal() +
+  
+  ## Adjust x ticks
+  theme(axis.text.x = element_text(angle = 45,
+                                   hjust = 1))
 
 ## Comments:
 ## - The lowest price is in February, at 1.27 USD.
@@ -301,33 +349,33 @@ avd_cleaned |>
 ## - Throughout a year, the average price rises steadily, dipping twice, in February and May, until reaching a peak in October and then drops steadily for the rest of the year.
 
 
-## 3.3 Price vs region
+## 3.3 Price vs region group
 avd_cleaned |>
   
-  ## Group by region
-  group_by(Region.Group) |>
+  ## Group by region group
+  group_by(region_group) |>
   
-  ## Compute mean price per region
-  summarise(MeanPrice = mean(AveragePrice)) |>
+  ## Compute mean price per region group
+  summarise(mean_price = mean(avg_price)) |>
   
   ## Ungroup
   ungroup() |>
   
   ## Order by mean price
-  mutate(Region.Group = fct_reorder(Region.Group,
-                                    MeanPrice,
+  mutate(region_group = fct_reorder(region_group,
+                                    mean_price,
                                     .desc = TRUE)) |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = Region.Group,
-             y = MeanPrice,
-             fill = Region.Group)) +
+  ggplot(aes(x = region_group,
+             y = mean_price,
+             fill = region_group)) +
   
   ## Instantiate a bar plot
   geom_col() +
   
   ## Add annotation
-  geom_text(aes(label = paste(round(MeanPrice, 2), "USD")),
+  geom_text(aes(label = paste(round(mean_price, 2), "USD")),
             position = position_dodge(width = 0.9),
             vjust = -0.5,
             size = 3) +
@@ -345,22 +393,22 @@ avd_cleaned |>
 ## - The other regions have similar pricing levels (between 1.32 and 1.39 USD).
 
 
-## 3.4 Price vs type
+## 3.4 Price vs pricing strategy
 avd_cleaned |>
   
-  ## Group by type
-  group_by(type) |>
+  ## Group by pricing strategy
+  group_by(pricing_strategy) |>
   
-  ## Compute mean price per type
-  summarise(MeanPrice = mean(AveragePrice)) |>
+  ## Compute mean price per pricing strategy
+  summarise(mean_price = mean(avg_price)) |>
   
   ## Ungroup
   ungroup() |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = type,
-             y = MeanPrice,
-             fill = type)) +
+  ggplot(aes(x = pricing_strategy,
+             y = mean_price,
+             fill = pricing_strategy)) +
   
   ## Instantiate a line plot
   geom_col() +
@@ -380,21 +428,21 @@ avd_cleaned |>
 
 ### 3.5 Price vs volume
 
-## Explore the relationship between `Total.Volume` and `AveragePrice`
+## 3.5.1 Explore the relationship between price and total volume
 avd_cleaned |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = Total.Volume,
-             y = AveragePrice)) +
+  ggplot(aes(x = vol_total,
+             y = avg_price)) +
   
   ## Instantiate a scatter plot
   geom_point(position = "jitter",
              alpha = 0.5,
-             color = "orange") +
+             color = "#7ea122") +
   
   ## Add a trend line
   geom_smooth(method = "loess",
-              color = "blue",
+              color = "red",
               se = FALSE) +
   
   ## Add text elements
@@ -410,30 +458,30 @@ avd_cleaned |>
 
 
 ## Conduct correlation test
-cor.test(avd_cleaned$AveragePrice,
-         avd_cleaned$Total.Volume,
+cor.test(avd_cleaned$avg_price,
+         avd_cleaned$vol_total,
          method = "pearson")
 
 ## Comments:
-## - There is a weakly yet significant negative correlation between price and volume such that as volume increases, price decreases.
+## - There is a weak yet significant negative correlation between price and volume such that as volume increases, price decreases.
 ## - The magnitude of the correlation suggests that volume may not play a major role in avocado pricing.
 
 
-## Explore potential moderators: Region
+## 3.5.2 Explore potential moderators: Region Group
 avd_cleaned |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = Total.Volume,
-             y = AveragePrice)) +
+  ggplot(aes(x = vol_total,
+             y = avg_price)) +
   
   ## Instantiate a scatter plot
   geom_point(position = "jitter",
              alpha = 0.5,
-             color = "orange") +
+             color = "#7ea122") +
   
   ## Add a trend line
   geom_smooth(method = "loess",
-              color = "blue",
+              color = "red",
               se = FALSE) +
   
   ## Add text elements
@@ -448,7 +496,7 @@ avd_cleaned |>
   scale_x_log10() +
   
   ## Facet by Region.Group
-  facet_wrap(~ Region.Group,
+  facet_wrap(~ region_group,
              scales = "free_y")
 
 ## Comments:
@@ -456,21 +504,21 @@ avd_cleaned |>
 ## - This suggests that region groups may not be a moderator of this relationship.
 
 
-## Explore potential moderators: Pricing strategy
+## 3.5.3 Explore potential moderators: Pricing strategy
 avd_cleaned |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = Total.Volume,
-             y = AveragePrice)) +
+  ggplot(aes(x = vol_total,
+             y = avg_price)) +
   
   ## Instantiate a scatter plot
   geom_point(position = "jitter",
              alpha = 0.5,
-             color = "orange") +
+             color = "#7ea122") +
   
   ## Add a trend line
   geom_smooth(method = "loess",
-              color = "blue",
+              color = "red",
               se = FALSE) +
   
   ## Add text elements
@@ -485,7 +533,7 @@ avd_cleaned |>
   scale_x_log10() +
   
   ## Facet by type
-  facet_wrap(~ type,
+  facet_wrap(~ pricing_strategy,
              scales = "free_y")
 
 ## Comments:
@@ -499,16 +547,17 @@ avd_cleaned |>
 avd_cleaned |>
   
   ## Group by type and major region
-  group_by(type, Region.Group) |>
+  group_by(pricing_strategy,
+           region_group) |>
   
   ## Compute mean price per type
-  summarise(MeanPrice = mean(AveragePrice),
+  summarise(mean_price = mean(avg_price),
             .groups = "drop") |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = type,
-             y = MeanPrice,
-             fill = type)) +
+  ggplot(aes(x = pricing_strategy,
+             y = mean_price,
+             fill = pricing_strategy)) +
   
   ## Instantiate a line plot
   geom_col() +
@@ -522,27 +571,28 @@ avd_cleaned |>
   theme_minimal() +
   
   ## Facet by region
-  facet_wrap(~ Region.Group)
+  facet_wrap(~ region_group)
 
 ## Comments:
 ## - Across all regions, organic pricing is consistently associated with higher price.
 ## - This suggests that there is no interaction effect between pricing strategy and region on pricing.
 
 
-## 3.6.1 Pricing strategy vs month
+## 3.6.2 Pricing strategy vs month
 avd_cleaned |>
   
   ## Group by type and major region
-  group_by(type, month) |>
+  group_by(pricing_strategy,
+           month) |>
   
   ## Compute mean price per type
-  summarise(MeanPrice = mean(AveragePrice),
+  summarise(mean_price = mean(avg_price),
             .groups = "drop") |>
   
   ## Aesthetic mapping
-  ggplot(aes(x = type,
-             y = MeanPrice,
-             fill = type)) +
+  ggplot(aes(x = pricing_strategy,
+             y = mean_price,
+             fill = pricing_strategy)) +
   
   ## Instantiate a line plot
   geom_col() +
@@ -568,16 +618,19 @@ avd_cleaned |>
 
 # 4. Building a Predictive Model
 
-## I will build two models, compare their performance, then choose the better performing one.
-## The models are linear regression and random forest.
-## Both are chosen due to their explainability.
+## I will build an exXtreme Gradient Boosting (XGBoost) model due to its high performance and explanability.
 
-## 4.1 Prepare the data
+## 4.1 Initial model
+
+## 4.1.1 Split the data
+
+## Set seed for reproducibility
+set.seed(888)
 
 ## Create splitting index
 avd_split <- initial_split(avd_cleaned,
                            prop = 0.8,
-                           strata = AveragePrice)
+                           strata = avg_price)
 
 ## Create a training set
 avd_train <- training(avd_split)
@@ -590,19 +643,147 @@ cat("Training set:", nrow(avd_train), "\n")
 cat("Test set:", nrow(avd_test))
 
 
-## 4.2 Define a recipe
-avd_recipe <- recipe(AveragePrice ~ .,
+## 4.1.2 Define the recipe
+avd_recipe <- recipe(avg_price ~ .,
                      avd_train) |>
+  
+  ## Remove redundant variables
+  step_rm(date, year, region,
+          vol_4046, vol_4225, vol_4770,
+          bags_small, bags_large, bags_xl) |>
   
   ## Normalise all numeric variables
   step_normalize(all_numeric()) |>
   
-  ## Handle multicollinearity
-  step_corr(all_numeric_predictors(),
-            threshold = 0.7) |>
-  
   ## Dummy-encode nominal variables
-  step_dummy(all_nominal()) |>
+  step_dummy(all_nominal())
+
+## Check the recipe
+avd_recipe
+
+
+## 4.1.3 Instantiate an XGBoost model
+xgb_mod <- boost_tree() |>
   
-  ## Remove redundant variables
-  step_rm("Date", "region")
+  ## Set engine
+  set_engine("xgboost") |>
+  
+  ## Set mode
+  set_mode("regression")
+
+
+## 4.1.4 Bundle the recipe and the model
+xgb_wfl <- workflow() |>
+  
+  ## Add recipe
+  add_recipe(avd_recipe) |>
+  
+  ## Add model
+  add_model(xgb_mod)
+
+
+## 4.1.5 Fit the model
+
+## Set the metrics
+xgb_metrics <-metric_set(mae,
+                         rmse,
+                         rsq)
+
+## Set seed for reproducibility
+set.seed(888)
+
+## Fit
+xgb_fit <- last_fit(xgb_wfl,
+                    split = avd_split,
+                    metrics = xgb_metrics)
+
+
+## 4.1.6 Evaluate the model
+
+## Collect metrics
+xgb_performance <- collect_metrics(xgb_fit)
+
+## Print the results
+xgb_performance
+
+# # A tibble: 3 × 4
+#   .metric .estimator .estimate .config             
+#   <chr>   <chr>          <dbl> <chr>               
+# 1 mae     standard       0.461 Preprocessor1_Model1
+# 2 rmse    standard       0.606 Preprocessor1_Model1
+# 3 rsq     standard       0.635 Preprocessor1_Model1
+
+## Comments
+## - The initial model demonstrated moderate performance, with MAE of 0.46 and RMSE of 0.61.
+## - The model was able to account for about 64% of the avocado price variance.
+
+
+## 4.7 Tune the model
+
+## 4.7.1 Define the hyperparametres for tuning
+xgb_mod_tune <- boost_tree(trees = tune(),
+                           tree_depth = tune(),
+                           learn_rate = tune(),
+                           sample_size = tune(),
+                           loss_reduction = tune()) |>
+  
+  ## Set engine
+  set_engine("xgboost") |>
+  
+  ## Set mode
+  set_mode("regression")
+
+
+## 4.7.2 Bundle the recipe and the model
+xgb_wfl_tune <- workflow() |>
+  
+  ## Add recipe
+  add_recipe(avd_recipe) |>
+  
+  ## Add model
+  add_model(xgb_mod_tune)
+
+
+## 4.7.3 Define resampling method
+
+## Set seed for reproducibility
+set.seed(888)
+
+## Set k-fold cross-validation
+xgb_cv <- vfold_cv(avd_train,
+                   v = 10,
+                   strata = avg_price)
+
+
+## 4.7.4 Create a hyperparametre grid
+
+## Define hyperparametre ranges
+xgb_hp <- parameters(trees() %>% range_set(c(100L, 1000L)),
+                     tree_depth() %>% range_set(c(3L, 10L)),
+                     learn_rate() %>% range_set(c(0.01, 0.3)),
+                     sample_prop() %>% range_set(c(0.5, 1.0)),
+                     loss_reduction() %>% range_set(c(0, 10)))
+
+## Define a random grid
+
+## Set seed for reproducibility
+set.seed(888)
+
+## Define
+xgb_hp_grid <- grid_random(xgb_hp,
+                           size = 30)
+
+
+## 4.7.5 Tune the model
+
+## Set seed for reproducibility
+set.seed(888)
+
+## Tune
+system.time({
+  xgb_tune_results <- tune_grid(xgb_wfl_tune,
+                                resamples = xgb_cv,
+                                grid = xgb_hp_grid,
+                                metrics = xgb_metrics,
+                                verbose = 1)
+})
